a46ab9eab7c077f95ee4ec7b063cc419049079fc
vaudois <info@vaudois.ch>
2022-09-05 01:36:57 +0200

    Update source.sh

--- utils/source.sh
+++ utils/source.sh
@@ -25,9 +25,14 @@ cd $HOME/utils/daemon_builder/temp_coin_builds
 # Get the github information
 read -r -e -p "Enter the name of the coin : " coin
 read -r -e -p "Paste the github link for the coin : " git_hub
+echo
+read -r -e -p "Switch from main to develop ? [y/N] :" swithdevelop
+if [[ ("$swithdevelop" == "N" || "$swithdevelop" == "n" || "$swithdevelop" == "not" || "$swithdevelop" == "NOT" || "$swithdevelop" == "no" || "$swithdevelop" == "NO" || "$swithdevelop" == "none" || "$swithdevelop" == "NONE") ]]; then
+echo
 read -r -e -p "Do you need to use a specific github branch of the coin (y/n) : " branch_git_hub
 if [[ ("$branch_git_hub" == "y" || "$branch_git_hub" == "Y" || "$branch_git_hub" == "yes" || "$branch_git_hub" == "Yes" || "$branch_git_hub" == "YES") ]]; then
 read -r -e -p "Please enter the branch name exactly as in github, i.e. v2.5.1  : " branch_git_hub_ver
+fi
 fi
 
 coindir=$coin$now
@@ -45,10 +50,19 @@ if [[ ("$branch_git_hub" == "y" || "$branch_git_hub" == "Y" || "$branch_git_hub"
   git fetch
   git checkout "$branch_git_hub_ver"
 fi
+if [[ ("$swithdevelop" == "y" || "$swithdevelop" == "Y" || "$swithdevelop" == "yes" || "$swithdevelop" == "Yes" || "$swithdevelop" == "YES") ]]; then
+  git checkout develop
+fi
+errorexist="false"
 else
 echo "$HOME/utils/daemon_builder/temp_coin_builds/${coindir} already exists.... Skipping"
 echo "If there was an error in the build use the build error options on the installer"
+errorexist="true"
 exit 0
+fi
+
+if [[("$errorexist" == "false")]]; then
+sudo chmod -R 777 $HOME/utils/daemon_builder/temp_coin_builds/${coindir}
 fi
 
 # Build the coin under the proper configuration
@@ -76,71 +90,213 @@ echo "Building using Berkeley 5.1..."
 basedir=$(pwd)
 sh autogen.sh
 if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/share/genbuild.sh' ]]; then
-  echo "genbuild.sh not found skipping"
-else
-sudo chmod 777 $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/share/genbuild.sh
-fi
-if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/leveldb/build_detect_platform' ]]; then
-  echo "build_detect_platform not found skipping"
-else
-sudo chmod 777 $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/leveldb/build_detect_platform
-fi
+    echo "genbuild.sh not found skipping"
+  else
+  sudo chmod 777 $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/share/genbuild.sh
+  fi
+  if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/leveldb/build_detect_platform' ]]; then
+    echo "build_detect_platform not found skipping"
+  else
+  sudo chmod 777 $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/leveldb/build_detect_platform
+  fi
 ./configure CPPFLAGS="-I${HOME}/utils/berkeley/db5/include -O2" LDFLAGS="-L${HOME}/utils/berkeley/db5/lib" --without-gui --disable-tests
 fi
 # Build the coin under berkeley 5.1
 if [[ ("$berkeley" == "5.3") ]]; then
-echo "Building using Berkeley 5.3..."
-basedir=$(pwd)
-sh autogen.sh
-if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/share/genbuild.sh' ]]; then
-  echo "genbuild.sh not found skipping"
-else
-sudo chmod 777 $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/share/genbuild.sh
-fi
-if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/leveldb/build_detect_platform' ]]; then
-  echo "build_detect_platform not found skipping"
-else
-sudo chmod 777 $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/leveldb/build_detect_platform
-fi
+  echo "Building using Berkeley 5.3..."
+  basedir=$(pwd)
+  sh autogen.sh
+  if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/share/genbuild.sh' ]]; then
+    echo "genbuild.sh not found skipping"
+  else
+  sudo chmod 777 $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/share/genbuild.sh
+  fi
+  if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/leveldb/build_detect_platform' ]]; then
+    echo "build_detect_platform not found skipping"
+  else
+  sudo chmod 777 $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/leveldb/build_detect_platform
+  fi
 ./configure CPPFLAGS="-I${HOME}/utils/berkeley/db5.3/include -O2" LDFLAGS="-L${HOME}/utils/berkeley/db5.3/lib" --without-gui --disable-tests
 fi
 # Build the coin under berkeley 6.2
 if [[ ("$berkeley" == "6.2") ]]; then
-echo "Building using Berkeley 6.2..."
-basedir=$(pwd)
-sh autogen.sh
-find . -maxdepth 1 -type d \( -perm -1 -o \( -perm -10 -o -perm -100 \) \) -printf "%f\n"
-read -r -e -p "where is the folder that contains the BUILD.SH installation file, example xxutil :" reputil
-cd $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/${reputil}
-echo $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/${reputil}
-./build.sh -j$(nproc)
-if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/${reputil}/fetch-params.sh' ]]; then
-  echo "fetch-params.sh not found skipping"
-else
-sh fetch-params.sh
+  echo "Building using Berkeley 6.2..."
+  basedir=$(pwd)
+  sh autogen.sh
+  find . -maxdepth 1 -type d \( -perm -1 -o \( -perm -10 -o -perm -100 \) \) -printf "%f\n"
+  read -r -e -p "where is the folder that contains the BUILD.SH installation file, example xxutil :" reputil
+  cd $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/${reputil}
+  echo $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/${reputil}
+  spiner_output bash build.sh -j$(nproc)
+  if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/${reputil}/fetch-params.sh' ]]; then
+    echo "fetch-params.sh not found skipping"
+  else
+    sh fetch-params.sh
+  fi
 fi
-else
+if [[ ("$berkeley" != "6.2") ]]; then
 # make install
 make -j$(nproc)
 fi
 else
 if [[ ("$cmake" == "true") ]]; then
-echo "Building using Cmake method..."
-cd $HOME/utils/daemon_builder/temp_coin_builds/${coindir} && git submodule init && git submodule update
-make -j$NPROC
-else
-echo "Building using makefile.unix method..."
-cd $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src
-if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/obj' ]]; then
-mkdir -p $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/obj
-else
-echo "Hey the developer did his job and the src/obj dir is there!"
+  clear
+  DEPENDS="$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/depends/"
+	if [ -d "$DEPENDS" ]; then
+		echo
+		echo
+		echo -e "$CYAN => Building using cmake with DEPENDS directory... $COL_RESET"
+		echo
+		sleep 3
+		
+		echo
+		echo
+		read -r -e -p "Hide LOG from to Work Coin ? [y/N] :" ifhidework
+		echo
+		
+		# Executing make on depends directory
+		echo
+		echo -e "$YELLOW => executing make on depends directory... $COL_RESET"
+		echo
+		sleep 3
+		cd $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/depends
+		if [[ ("$ifhidework" == "y" || "$ifhidework" == "Y") ]]; then
+		hide_output make -j$NPROC
+		else
+		make -j$NPROC
+		fi
+		echo
+		echo
+		echo -e "$GREEN Done...$COL_RESET"
+
+		# Building autogen....
+		echo
+		echo -e "$YELLOW => Building autogen... $COL_RESET"
+		echo
+		sleep 3
+		cd $HOME/utils/daemon_builder/temp_coin_builds/${coindir}
+		if [[ ("$ifhidework" == "y" || "$ifhidework" == "Y") ]]; then
+		hide_output sh autogen.sh
+		else
+		sh autogen.sh
+		fi
+		echo
+		echo
+		echo -e "$GREEN Done...$COL_RESET"
+
+		# Configure with your platform....
+		if [ -d "$DEPENDS/i686-pc-linux-gnu" ]; then
+			echo
+			echo -e "$YELLOW => Configure with i686-pc-linux-gnu... $COL_RESET"
+			echo
+			sleep 3
+			if [[ ("$ifhidework" == "y" || "$ifhidework" == "Y") ]]; then
+			hide_output ./configure --prefix=`pwd`/depends/i686-pc-linux-gnu
+			else
+			./configure --prefix=`pwd`/depends/i686-pc-linux-gnu
+			fi
+		elif [ -d "$DEPENDS/x86_64-pc-linux-gnu/" ]; then
+			echo
+			echo -e "$YELLOW => Configure with x86_64-pc-linux-gnu... $COL_RESET"
+			echo
+			sleep 3
+			if [[ ("$ifhidework" == "y" || "$ifhidework" == "Y") ]]; then
+			hide_output ./configure --prefix=`pwd`/depends/x86_64-pc-linux-gnu
+			else
+			./configure --prefix=`pwd`/depends/x86_64-pc-linux-gnu
+			fi
+		elif [ -d "$DEPENDS/i686-w64-mingw32/" ]; then
+			echo
+			echo -e "$YELLOW => Configure with i686-w64-mingw32... $COL_RESET"
+			echo
+			sleep 3
+			if [[ ("$ifhidework" == "y" || "$ifhidework" == "Y") ]]; then
+			hide_output ./configure --prefix=`pwd`/depends/i686-w64-mingw32
+			else
+			./configure --prefix=`pwd`/depends/i686-w64-mingw32
+			fi
+		elif [ -d "$DEPENDS/x86_64-w64-mingw32/" ]; then
+			echo
+			echo -e "$YELLOW => Configure with x86_64-w64-mingw32... $COL_RESET"
+			echo
+			sleep 3
+			if [[ ("$ifhidework" == "y" || "$ifhidework" == "Y") ]]; then
+			hide_output ./configure --prefix=`pwd`/depends/x86_64-w64-mingw32
+			else
+			./configure --prefix=`pwd`/depends/x86_64-w64-mingw32
+			fi
+		elif [ -d "$DEPENDS/x86_64-apple-darwin14/" ]; then
+			echo
+			echo -e "$YELLOW => Configure with x86_64-apple-darwin14... $COL_RESET"
+			echo
+			sleep 3
+			if [[ ("$ifhidework" == "y" || "$ifhidework" == "Y") ]]; then
+			hide_output ./configure --prefix=`pwd`/depends/x86_64-apple-darwin14
+			else
+			./configure --prefix=`pwd`/depends/x86_64-apple-darwin14
+			fi
+		elif [ -d "$DEPENDS/arm-linux-gnueabihf/" ]; then
+			echo
+			echo -e "$YELLOW => Configure with arm-linux-gnueabihf... $COL_RESET"
+			echo
+			sleep 3
+			if [[ ("$ifhidework" == "y" || "$ifhidework" == "Y") ]]; then
+			hide_output ./configure --prefix=`pwd`/depends/arm-linux-gnueabihf
+			else
+			./configure --prefix=`pwd`/depends/arm-linux-gnueabihf
+			fi
+		elif [ -d "$DEPENDS/aarch64-linux-gnu/" ]; then
+			echo
+			echo -e "$YELLOW => Configure with aarch64-linux-gnu... $COL_RESET"
+			echo
+			sleep 3
+			if [[ ("$ifhidework" == "y" || "$ifhidework" == "Y") ]]; then
+			hide_output ./configure --prefix=`pwd`/depends/aarch64-linux-gnu
+			else
+			./configure --prefix=`pwd`/depends/aarch64-linux-gnu
+			fi
+		fi
+		echo
+		echo
+		echo -e "$GREEN Done...$COL_RESET"
+		
+		# Executing make to finalize....
+		echo
+		echo -e "$YELLOW => Executing make to finalize... $COL_RESET"
+		echo
+		sleep 3
+		if [[ ("$ifhidework" == "y" || "$ifhidework" == "Y") ]]; then
+		hide_output make -j$NPROC
+		else
+		make -j$NPROC
+		fi
+		echo
+		echo
+		echo -e "$GREEN Done...$COL_RESET"
+	else
+		echo
+		echo "Building using Cmake method..."
+		echo
+		echo
+		sleep 3
+		cd $HOME/utils/daemon_builder/temp_coin_builds/${coindir} && git submodule init && git submodule update
+		make -j$NPROC
+		sleep 3
+	fi
 fi
-if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/obj/zerocoin' ]]; then
-mkdir -p $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/obj/zerocoin
-else
-echo  "Wow even the /src/obj/zerocoin is there! Good job developer!"
-fi
+if [[ ("$unix" == "true") ]]; then
+  echo "Building using makefile.unix method..."
+  cd $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src
+  if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/obj' ]]; then
+    mkdir -p $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/obj
+  else
+    echo "Hey the developer did his job and the src/obj dir is there!"
+  fi
+  if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/obj/zerocoin' ]]; then
+    mkdir -p $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/obj/zerocoin
+  else
+    echo  "Wow even the /src/obj/zerocoin is there! Good job developer!"
+  fi
 cd $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/leveldb
 sudo chmod +x build_detect_platform
 sudo make clean
@@ -200,13 +356,13 @@ clear
 sudo strip $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coind}
 sudo cp $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coind} /usr/bin
 if [[ ("$ifcoincli" == "y" || "$ifcoincli" == "Y") ]]; then
-sudo strip $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coincli}
-sudo cp $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coincli} /usr/bin
+  sudo strip $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coincli}
+  sudo cp $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coincli} /usr/bin
 fi
 
 # Make the new wallet folder have user paste the coin.conf and finally start the daemon
 if [[ ! -e '$HOME/wallets' ]]; then
-sudo mkdir -p $HOME/wallets
+  sudo mkdir -p $HOME/wallets
 fi
 
 sudo setfacl -m u:$USER:rwx $HOME/wallets

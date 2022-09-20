c3106932cf4499aeb7e736459844cd8aa5ae93d4
vaudois <info@vaudois.ch>
2022-09-20 22:05:10 +0200

    Update source.sh

--- utils/source.sh
+++ utils/source.sh
@@ -3,7 +3,13 @@
 # Created by afiniel for crypto use...
 #####################################################
 
-source /etc/functions.sh
+FUNC=/etc/functionscoin.sh
+if [[ ! -f "$FUNC" ]]; then
+	source /etc/functions.sh
+else
+	source /etc/functionscoin.sh
+fi
+
 source $HOME/utils/daemon_builder/.my.cnf
 cd $HOME/utils/daemon_builder
 
@@ -12,9 +18,9 @@ now=$(date +"%m_%d_%Y")
 set -e
 NPROC=$(nproc)
 if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds' ]]; then
-sudo mkdir -p $HOME/utils/daemon_builder/temp_coin_builds
+	sudo mkdir -p $HOME/utils/daemon_builder/temp_coin_builds
 else
-echo "temp_coin_builds already exists.... Skipping"
+	echo "temp_coin_builds already exists.... Skipping"
 fi
 
 # Just double checking folder permissions
@@ -24,15 +30,20 @@ cd $HOME/utils/daemon_builder/temp_coin_builds
 
 # Get the github information
 read -r -e -p "Enter the name of the coin : " coin
-read -r -e -p "Paste the github link for the coin : " git_hub
-echo
-read -r -e -p "Switch from main to develop ? [y/N] :" swithdevelop
-if [[ ("$swithdevelop" == "N" || "$swithdevelop" == "n" || "$swithdevelop" == "not" || "$swithdevelop" == "NOT" || "$swithdevelop" == "no" || "$swithdevelop" == "NO" || "$swithdevelop" == "none" || "$swithdevelop" == "NONE") ]]; then
-echo
-read -r -e -p "Do you need to use a specific github branch of the coin (y/n) : " branch_git_hub
-if [[ ("$branch_git_hub" == "y" || "$branch_git_hub" == "Y" || "$branch_git_hub" == "yes" || "$branch_git_hub" == "Yes" || "$branch_git_hub" == "YES") ]]; then
-read -r -e -p "Please enter the branch name exactly as in github, i.e. v2.5.1  : " branch_git_hub_ver
-fi
+if [[ ("$precompiled" == "true") ]]; then
+	read -r -e -p "Paste the github link coin precompiled *.tar.gz OR *.zip: " coin_precompiled
+	echo
+else
+	read -r -e -p "Paste the github link for the coin : " git_hub
+	echo
+	read -r -e -p "Switch from main to develop ? [y/N] :" swithdevelop
+	if [[ ("$swithdevelop" == "N" || "$swithdevelop" == "n" || "$swithdevelop" == "not" || "$swithdevelop" == "NOT" || "$swithdevelop" == "no" || "$swithdevelop" == "NO" || "$swithdevelop" == "none" || "$swithdevelop" == "NONE") ]]; then
+		echo
+		read -r -e -p "Do you need to use a specific github branch of the coin (y/n) : " branch_git_hub
+		if [[ ("$branch_git_hub" == "y" || "$branch_git_hub" == "Y" || "$branch_git_hub" == "yes" || "$branch_git_hub" == "Yes" || "$branch_git_hub" == "YES") ]]; then
+			read -r -e -p "Please enter the branch name exactly as in github, i.e. v2.5.1  : " branch_git_hub_ver
+		fi
+	fi
 fi
 
 coindir=$coin$now
@@ -44,21 +55,29 @@ lastcoin='"${coindir}"'
 
 # Clone the coin
 if [[ ! -e $coindir ]]; then
-git clone $git_hub $coindir
-cd "${coindir}"
-if [[ ("$branch_git_hub" == "y" || "$branch_git_hub" == "Y" || "$branch_git_hub" == "yes" || "$branch_git_hub" == "Yes" || "$branch_git_hub" == "YES") ]]; then
-  git fetch
-  git checkout "$branch_git_hub_ver"
-fi
-if [[ ("$swithdevelop" == "y" || "$swithdevelop" == "Y" || "$swithdevelop" == "yes" || "$swithdevelop" == "Yes" || "$swithdevelop" == "YES") ]]; then
-  git checkout develop
-fi
-errorexist="false"
+	if [[ ("$precompiled" == "true") ]]; then
+		mkdir $coindir
+		cd "${coindir}"
+		sudo wget $coin_precompiled
+	else
+		git clone $git_hub $coindir
+		cd "${coindir}"
+	fi
+
+	if [[ ("$branch_git_hub" == "y" || "$branch_git_hub" == "Y" || "$branch_git_hub" == "yes" || "$branch_git_hub" == "Yes" || "$branch_git_hub" == "YES") ]]; then
+	  git fetch
+	  git checkout "$branch_git_hub_ver"
+	fi
+
+	if [[ ("$swithdevelop" == "y" || "$swithdevelop" == "Y" || "$swithdevelop" == "yes" || "$swithdevelop" == "Yes" || "$swithdevelop" == "YES") ]]; then
+	  git checkout develop
+	fi
+	errorexist="false"
 else
-echo "$HOME/utils/daemon_builder/temp_coin_builds/${coindir} already exists.... Skipping"
-echo "If there was an error in the build use the build error options on the installer"
-errorexist="true"
-exit 0
+	echo "$HOME/utils/daemon_builder/temp_coin_builds/${coindir} already exists.... Skipping"
+	echo "If there was an error in the build use the build error options on the installer"
+	errorexist="true"
+	exit 0
 fi
 
 if [[("$errorexist" == "false")]]; then
@@ -141,7 +160,7 @@ fi
 else
 if [[ ("$cmake" == "true") ]]; then
   clear
-  DEPENDS="$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/depends/"
+  DEPENDS="$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/depends"
 	if [ -d "$DEPENDS" ]; then
 		echo
 		echo
@@ -284,19 +303,21 @@ if [[ ("$cmake" == "true") ]]; then
 		sleep 3
 	fi
 fi
-if [[ ("$unix" == "true") ]]; then
-  echo "Building using makefile.unix method..."
-  cd $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src
-  if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/obj' ]]; then
-    mkdir -p $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/obj
-  else
-    echo "Hey the developer did his job and the src/obj dir is there!"
-  fi
-  if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/obj/zerocoin' ]]; then
-    mkdir -p $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/obj/zerocoin
-  else
-    echo  "Wow even the /src/obj/zerocoin is there! Good job developer!"
-  fi
+	if [[ ("$unix" == "true") ]]; then
+		echo "Building using makefile.unix method..."
+		cd $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src
+		if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/obj' ]]; then
+			mkdir -p $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/obj
+		else
+			echo "Hey the developer did his job and the src/obj dir is there!"
+		fi
+
+		if [[ ! -e '$HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/obj/zerocoin' ]]; then
+			mkdir -p $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/obj/zerocoin
+		else
+			echo  "Wow even the /src/obj/zerocoin is there! Good job developer!"
+		fi
+
 		cd $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/leveldb
 		sudo chmod +x build_detect_platform
 		sudo make clean
@@ -308,67 +329,116 @@ if [[ ("$unix" == "true") ]]; then
 	fi
 fi
 
+if [[ ("$precompiled" == "true") ]]; then
+	COINTARGZ=$(find ~+ -type f -name "*.tar.gz")
+	COINZIP=$(find ~+ -type f -name "*.zip")
+	if [[ -f "$COINZIP" ]]; then
+		for i in $(ls -f *.zip); do coinzipped=${i%%}; done
+		sudo unzip -q $coinzipped -d newcoin
+		for i in $(ls -d */); do repzipcoin=${i%%/}; done
+	elif [[ -f "$COINTARGZ" ]]; then
+		for i in $(ls -f *.tar.gz); do coinzipped=${i%%}; done
+		sudo tar xzvf $coinzipped
+		for i in $(ls -d */); do repzipcoin=${i%%/}; done
+	else
+		echo -e "$RED => This is a not valid file zipped $COL_RESET"
+	fi
+fi
+
 clear
 
 # LS the SRC dir to have user input bitcoind and bitcoin-cli names
-cd $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/
-find . -maxdepth 1 -type f ! -name "*.*" \( -perm -1 -o \( -perm -10 -o -perm -100 \) \) -printf "%f\n"
-read -r -e -p "Please enter the coind name from the directory above, example bitcoind :" coind
-read -r -e -p "Is there a coin-cli, example bitcoin-cli [y/N] :" ifcoincli
+if [[ ! ("$precompiled" == "true") ]]; then
 
-if [[ ("$ifcoincli" == "y" || "$ifcoincli" == "Y") ]]; then
-	read -r -e -p "Please enter the coin-cli name :" coincli
-fi
+	cd $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/
+	
+	find . -maxdepth 1 -type f ! -name "*.*" \( -perm -1 -o \( -perm -10 -o -perm -100 \) \) -printf "%f\n"
+	read -r -e -p "Please enter the coind name from the directory above, example bitcoind :" coind
+	read -r -e -p "Is there a coin-cli, example bitcoin-cli [y/N] :" ifcoincli
 
-read -r -e -p "Is there a coin-tx, example bitcoin-tx [y/N] :" ifcointx
+		if [[ ("$ifcoincli" == "y" || "$ifcoincli" == "Y") ]]; then
+			read -r -e -p "Please enter the coin-cli name :" coincli
+		fi
 
-if [[ ("$ifcointx" == "y" || "$ifcointx" == "Y") ]]; then
-	read -r -e -p "Please enter the coin-tx name :" cointx
-fi
+		read -r -e -p "Is there a coin-tx, example bitcoin-tx [y/N] :" ifcointx
 
+		if [[ ("$ifcointx" == "y" || "$ifcointx" == "Y") ]]; then
+			read -r -e -p "Please enter the coin-tx name :" cointx
+		fi
 
-if [[ ("$berkeley" == "6.2") ]]; then
-	read -r -e -p "Is there a coin-tools, example bitcoin-wallet-tools [y/N] :" ifcointools
+		if [[ ("$berkeley" == "6.2" || "$precompiled" == "true") ]]; then
+			read -r -e -p "Is there a coin-tools, example bitcoin-wallet-tools [y/N] :" ifcointools
 
-	if [[ ("$ifcointools" == "y" || "$ifcointools" == "Y") ]]; then
-		read -r -e -p "Please enter the coin-tools name :" cointools
+		if [[ ("$ifcointools" == "y" || "$ifcointools" == "Y") ]]; then
+			read -r -e -p "Please enter the coin-tools name :" cointools
+		fi
+
+		read -r -e -p "Is there a coin-gtest, example bitcoin-gtest [y/N] :" ifcoingtest
+
+		if [[ ("$ifcoingtest" == "y" || "$ifcoingtest" == "Y") ]]; then
+			read -r -e -p "Please enter the coin-gtest name :" coingtest
+		fi
 	fi
 
-	read -r -e -p "Is there a coin-gtest, example bitcoin-gtest [y/N] :" ifcoingtest
-
-	if [[ ("$ifcoingtest" == "y" || "$ifcoingtest" == "Y") ]]; then
-		read -r -e -p "Please enter the coin-gtest name :" coingtest
-	fi
 fi
 
 clear
 
 # Strip and copy to /usr/bin
-sudo strip $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coind}
-sudo cp $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coind} /usr/bin
-if [[ ("$ifcoincli" == "y" || "$ifcoincli" == "Y") ]]; then
-	sudo strip $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coincli}
-	sudo cp $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coincli} /usr/bin
-fi
+if [[ ("$precompiled" == "true") ]]; then
+	cd $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/${repzipcoin}/
+	
+	COINDFIND=$(find ~+ -type f -name "*d")
+	COINCLIFIND=$(find ~+ -type f -name "*-cli")
+	COINTXFIND=$(find ~+ -type f -name "*-tx")
+	
+	coind=$(basename $COINDFIND)
 
-if [[ ("$ifcointx" == "y" || "$ifcointx" == "Y") ]]; then
-	sudo strip $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${cointx}
-	sudo cp $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${cointx} /usr/bin
-fi
+	sudo strip $COINDFIND
+	sudo cp $COINDFIND /usr/bin
+	sudo chmod +x /usr/bin/${coind}
+	
+	if [[ -f "$COINCLIFIND" ]]; then
+		coincli=$(basename $COINCLIFIND)
+		sudo strip $COINCLIFIND
+		sudo cp $COINCLIFIND /usr/bin
+		sudo chmod +x /usr/bin/${coincli}
+	fi
 
-if [[ ("$ifcointools" == "y" || "$ifcointools" == "Y") ]]; then
-	sudo strip $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${cointools}
-	sudo cp $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${cointools} /usr/bin
-fi
+	if [[ -f "$COINTXFIND" ]]; then
+		cointx=$(basename $COINTXFIND)
+		sudo strip $COINTXFIND
+		sudo cp $COINTXFIND /usr/bin
+		sudo chmod +x /usr/bin/${cointx}
+	fi
+else
+	sudo strip $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coind}
+	sudo cp $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coind} /usr/bin
 
-if [[ ("$ifcoingtest" == "y" || "$ifcoingtest" == "Y") ]]; then
-	sudo strip $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coingtest}
-	sudo cp $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coingtest} /usr/bin
+	if [[ ("$ifcoincli" == "y" || "$ifcoincli" == "Y") ]]; then
+	  sudo strip $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coincli}
+	  sudo cp $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coincli} /usr/bin
+	fi
+
+	if [[ ("$ifcointx" == "y" || "$ifcointx" == "Y") ]]; then
+		sudo strip $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${cointx}
+		sudo cp $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${cointx} /usr/bin
+	fi
+
+	if [[ ("$ifcoingtest" == "y" || "$ifcoingtest" == "Y") ]]; then
+		sudo strip $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coingtest}
+		sudo cp $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${coingtest} /usr/bin
+	fi
+
+	if [[ ("$ifcointools" == "y" || "$ifcointools" == "Y") ]]; then
+		sudo strip $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${cointools}
+		sudo cp $HOME/utils/daemon_builder/temp_coin_builds/${coindir}/src/${cointools} /usr/bin
+	fi
 fi
 
 # Make the new wallet folder have user paste the coin.conf and finally start the daemon
 if [[ ! -e '$HOME/wallets' ]]; then
-	sudo mkdir -p $HOME/wallets
+  sudo mkdir -p $HOME/wallets
 fi
 
 sudo setfacl -m u:$USER:rwx $HOME/wallets

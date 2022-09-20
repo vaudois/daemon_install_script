3ce20255013aeccecfee785b2c63df15055480e6
vaudois <info@vaudois.ch>
2022-09-20 22:07:24 +0200

    Update menu2.sh

--- utils/menu2.sh
+++ utils/menu2.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 #####################################################
-# Source code https://github.com/end222/pacmenu
-# Updated by afiniel for crypto use...
+# Updated by Vaudois for crypto use...
 #####################################################
 
-source /etc/functions.sh
+FUNC=/etc/functionscoin.sh
+if [[ ! -f "$FUNC" ]]; then
+	source /etc/functions.sh
+else
+	source /etc/functionscoin.sh
+fi
 cd $HOME/utils/daemon_builder
 
 RESULT=$(dialog --stdout --title "Daemon Builder Coin" --menu "Choose one" -1 60 7 \
